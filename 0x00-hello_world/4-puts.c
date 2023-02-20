67424c15f473659d8e24cc4ca02f6303a3a04b75
Tembeka <thembeka.zulu96@gmail.com>
2023-02-20 02:21:50 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <sdtio.h>
+/**
+ * main - A program that prints with PUTS function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
