2e9e3a70060d58da1b25207e826d93e3f0ac0a19
Tembeka <thembeka.zulu96@gmail.com>
2023-02-22 16:59:45 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i, z = 0;
 
-	while (i <1024)
+	while (i < 1024)
 	{
 	if ((i % 3 == 0) || (i % 5 == 0))
 	{
@@ -19,4 +19,3 @@ int main(void)
 	printf("%d\n", z);
 	return (0);
 }
-	
