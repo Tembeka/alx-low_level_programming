b7e6b8f922c10bf049b6464a1eda1caaf7a562c9
Tembeka <thembeka.zulu96@gmail.com>
2023-02-24 15:29:12 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif
