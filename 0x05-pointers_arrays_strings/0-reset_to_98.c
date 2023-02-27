5da5123e43d0eb498e1a9b22f1f96c61cdca3756
Tembeka <thembeka.zulu96@gmail.com>
2023-02-27 04:52:34 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * void reset_to_98  - Take a pointer ti an int
+ * and updates the value it points to 98
+ * @n: An input pointer
+ * Return: nothing!
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
