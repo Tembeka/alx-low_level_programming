14e269852f379923e5e950aab1b5db41349a9442
Tembeka <thembeka.zulu96@gmail.com>
2023-03-09 12:20:18 -0800

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+#ifndef _MAIN_
+#define _MAIN_
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -5,5 +7,11 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int _is_prime(int n, int x);
 int _putchar(char c);
 int _isqrt(int x, int y);
+int wildcmp(char *s1, char *s2);
+int _compare(char *s, int start, int end);
+int _strlen(char *s);
+int is_palindrome(char *s);
+#endif
