dd0ae6be0406fb1e5421e52e4992c9525963b7e4
Tembeka <thembeka.zulu96@gmail.com>
2023-03-10 13:08:30 -0800

    static_libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen - Returns length of a string
+ * @s: string
+ * Return: Length
+ */
+
+int _strlen(char *s)
+{
+        int longi = 0;
+
+        while (*s != '\0')
+        {
+        longi++;
+        s++;
+        }
+        return (longi);
+}
