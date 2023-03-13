426231dbc45a7dad36a33efdc7bcda3232529e57
Tembeka <thembeka.zulu96@gmail.com>
2023-03-13 11:41:54 -0700

    main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * main - Program that prints its name, followed by a new line
+ * return: nothing!
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar("%s\n", argv[0]);
+	return (0);
+}
