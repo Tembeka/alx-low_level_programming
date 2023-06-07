4e79e880ccac781e62a2a2d7118fd6076d5a2632
Tembeka <thembeka.zulu96@gmail.com>
2023-06-07 05:03:29 -0700

    header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,3 @@
-#ifndef _MAIN_H_
-#ifdef _MAIN_H_
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,4 +10,3 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
 
-#endif
