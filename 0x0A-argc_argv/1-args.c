de81d8d9dd7d9c3ae0b4b2cc7ca1f8074c428063
Tembeka <thembeka.zulu96@gmail.com>
2023-06-09 03:04:55 -0700

    number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of command-line arguments passed
+ * @argv: arrays of string
+ *
+ * Return:0 on success.
+ */
+
+int main(int argc, __attribute__((unused)) char  *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
