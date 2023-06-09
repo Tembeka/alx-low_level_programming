fc320febcd60c37502ba79a8d275b35750f22588
Tembeka <thembeka.zulu96@gmail.com>
2023-06-09 02:29:38 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of command-line arguments passed
+ * @argv: array of strings
+ *
+ * Return: 0 on success.
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
