701c9f93ab34ec351bf3c58110b9b45b78e72b7f
Tembeka <thembeka.zulu96@gmail.com>
2023-06-19 15:23:04 -0700

    header file

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,7 +1,15 @@
 #define MAIN_H
 #ifdef MAIN_H
 
-typedef dog dog;
+/**
+  * struct dog - Dog attributes
+  * @name: dog name.
+  * @age: dog age.
+  * @owner: owner of the dog.
+  *
+  * Description: attributes of a dog.
+  */
+
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
