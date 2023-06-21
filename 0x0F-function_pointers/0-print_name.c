74e0172c83d086b78ef407aa0e2a67a39510ab71
Tembeka <thembeka.zulu96@gmail.com>
2023-06-21 15:43:13 -0700

    prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - Write a function that prints a name.
+ *@name: character to be printed
+ *@f: pointer to the string
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
