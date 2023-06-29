b72f8461c94724a56d106b014c67d47f2728f8d6
Tembeka <thembeka.zulu96@gmail.com>
2023-06-29 16:14:51 -0700

    mmmh

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns num of elements in a linked list.
+ * @h: list to print
+ *
+ * Return: num of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
